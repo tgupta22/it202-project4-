aee4edf03bf77f175133976f011eccac2b38111e
Tarush Gupta <tgupta22@uic.edu>
2019-12-06 07:17:39 +0000

    Added index.html file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
